b47d4a191e1d97a74eb761960f40652df16ce38e
Tomas Plevko <tplevko@redhat.com>
2018-05-28 11:21:27 +0200

    aaa

--- src/main/java/io/swagger/sample/Application.java
+++ src/main/java/io/swagger/sample/Application.java
@@ -23,7 +23,6 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static com.google.common.collect.Sets.newHashSet;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -43,7 +42,7 @@ public class Application {
 
     @Bean
     public Docket swaggerSpringMvcPlugin() {
-        return new Docket(DocumentationType.SWAGGER_2).protocols(newHashSet("http", "https"))
+        return new Docket(DocumentationType.SWAGGER_2).protocols(newHashSet("http"))
             .useDefaultResponseMessages(false)
             .apiInfo(apiInfo())
             .select()
